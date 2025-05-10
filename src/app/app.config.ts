72c91951fcf7162a1007a87034a3116f3e43a50d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-10 01:36:30 -0300

    chore: providing api url in injection token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,21 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { API_URL } from './core/auth/tokens/api.token';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    {
+      provide: API_URL,
+      useValue: environment.apiUrl
+    }
+  ],
 };
