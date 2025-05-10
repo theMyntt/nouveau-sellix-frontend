3372f9e12f43252b2ea2677939ccd927cf693861
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-10 16:24:00 -0300

    fix: factory verifying in ssr

--- src/app/core/auth/tokens/storage.token.ts
+++ src/app/core/auth/tokens/storage.token.ts
@@ -2,5 +2,18 @@ import { InjectionToken } from "@angular/core";
 
 export const STORAGE_TOKEN = new InjectionToken<Storage>('Storage', {
     providedIn: 'root',
-    factory: () => localStorage
+    factory: () => {
+        if (typeof window !== 'undefined' && window.localStorage) {
+            return window.localStorage;
+        }
+
+        return {
+            length: 0,
+            clear: () => { },
+            getItem: () => null,
+            key: () => null,
+            removeItem: () => { },
+            setItem: () => { },
+        } as Storage;
+    }
 });
