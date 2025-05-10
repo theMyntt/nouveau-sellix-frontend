ddfaea8a111d217879317a24f7db3687def09b09
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-10 00:43:24 -0300

    feat: is auth guard

---
+++ src/app/core/auth/guards/is-authenticated.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, RedirectCommand, Router } from '@angular/router';
+import { CurrentUserLoggedStore } from '../stores/current-user-logged-store.store';
+
+export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+  const store = inject(CurrentUserLoggedStore)
+  const router = inject(Router)
+
+  if (!store.isLoggedIn()) {
+    const loginPath = router.parseUrl("/login")
+    return new RedirectCommand(loginPath)
+  }
+
+  return true;
+};
