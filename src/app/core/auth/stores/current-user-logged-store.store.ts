4b328f42018bc491cdcddbd2ba78ee8f97d0ef09
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-10 01:26:55 -0300

    feat: current user logged store methods

--- src/app/core/auth/stores/current-user-logged-store.store.ts
+++ src/app/core/auth/stores/current-user-logged-store.store.ts
@@ -1,13 +1,27 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, Signal, signal } from '@angular/core';
+import { iUser } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrentUserLoggedStore {
+  private user = signal<iUser | null>(null);
 
   constructor() { }
 
   public isLoggedIn(): boolean {
-    return true; //mock response
+    return this.user() != null;
+  }
+
+  public getCurrentUser(): Signal<iUser | null> {
+    return computed(() => this.user())
+  }
+
+  public logout(): void {
+    this.user.set(null)
+  }
+
+  public setUser(user: iUser): void {
+    this.user.set(user)
   }
 }
