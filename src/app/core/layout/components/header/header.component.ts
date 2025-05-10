80e4178a058aec03065b938fa758a9ced39c2df9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-10 15:23:06 -0300

    feat: header component

--- src/app/core/layout/components/header/header.component.ts
+++ src/app/core/layout/components/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { CurrentUserLoggedStore } from '../../../auth/stores/current-user-logged-store.store';
+import { iUser } from '../../../auth/interfaces/user.interface';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu'
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, MatMenuModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
+export class HeaderComponent{
+  protected readonly currentUserLoggedStore = inject(CurrentUserLoggedStore);
 }
