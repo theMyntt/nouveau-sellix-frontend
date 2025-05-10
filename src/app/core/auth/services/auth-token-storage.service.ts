0b4b2eb73912586d829f5dafa283864a8455d36b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-10 00:49:54 -0300

    feat: auth token storage service

---
+++ src/app/core/auth/services/auth-token-storage.service.ts
@@ -0,0 +1,28 @@
+import { inject, Injectable } from '@angular/core';
+import { STORAGE_TOKEN } from '../tokens/storage.token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthTokenStorageService {
+  private readonly storageKey = "auth-token"
+  private readonly storage = inject(STORAGE_TOKEN)
+
+  constructor() { }
+
+  public set(token: string): void {
+    this.storage.setItem(this.storageKey, token)
+  }
+
+  public get(): string | null {
+    return this.storage.getItem(this.storageKey)
+  }
+
+  public remove(): void {
+    this.storage.removeItem(this.storageKey)
+  }
+
+  public has(): Boolean {
+    return new Boolean(this.storage.getItem(this.storageKey))
+  }
+}
