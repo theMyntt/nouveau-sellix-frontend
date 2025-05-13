a7ab55adba45db709203e9972b8b30d750b0ef6d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-05-13 14:36:24 -0300

    feat: grid tile seconds

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatGridListModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
