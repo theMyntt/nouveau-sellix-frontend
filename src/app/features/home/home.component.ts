845a671148d3a38a13d40dce7016900c3ad6435d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-13 23:34:17 -0300

    feat: timer on widget

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,13 +1,31 @@
-import { Component, inject, signal } from '@angular/core';
+import { DatePipe, isPlatformBrowser } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list'
 
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule, MatButtonModule],
+  imports: [MatGridListModule, MatButtonModule, DatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  protected readonly time = signal<Date>(new Date());
 
+  private intervalId: any;
+  private readonly platformId = inject(PLATFORM_ID);
+
+  public ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.intervalId = setInterval(() => {
+        this.time.set(new Date());
+      }, 1000);
+    }
+  }
+
+  public ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
